204b8dae7c0010fad84c0d4447ddad07bbe0d6b3
Ramani-priya <168r1a04k4@cmrec.ac.in>
2020-08-20 16:53:49 +0530

    deploy

---
+++ public/precache-manifest.69d99fe01c77c0d6b862c76fcf46e9f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6b431fb34505a65cfda02538ebee97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fe215e277e39d4adc22",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "39f69d9204035b2fdbf9",
+    "url": "/static/css/main.e25c6442.chunk.css"
+  },
+  {
+    "revision": "6fe215e277e39d4adc22",
+    "url": "/static/js/2.92622544.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.92622544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f69d9204035b2fdbf9",
+    "url": "/static/js/main.ce1ff526.chunk.js"
+  },
+  {
+    "revision": "54b43ed2bee60737cb2a",
+    "url": "/static/js/runtime-main.b2162ddc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
